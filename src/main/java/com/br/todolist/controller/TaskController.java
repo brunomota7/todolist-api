876159cd2e74f50eeee4e97b3eb4220d9b34eb50
Bruno Mota <bruno7motadev@gmail.com>
2025-05-29 09:50:56 -0300

    feat: Finalizando TaskController e seus endpoints

--- src/main/java/com/br/todolist/controller/TaskController.java
+++ src/main/java/com/br/todolist/controller/TaskController.java
@@ -1,15 +1,69 @@
 package com.br.todolist.controller;
 
+import com.br.todolist.dto.request.TaskRequest;
+import com.br.todolist.dto.response.TaskResponse;
 import com.br.todolist.service.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addNewTask(@RequestBody @Valid TaskRequest task) {
+        taskService.addNewTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> getAllTask() {
+        List<TaskResponse> listTask = taskService.getAllTask();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(listTask);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponse> getTaskByID(@PathVariable Long id) {
+        TaskResponse task = taskService.getTaskById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(task);
+    }
+
+    @GetMapping("/completed")
+    public ResponseEntity<List<TaskResponse>> getAllTaskCompleted() {
+        List<TaskResponse> taskCompleted = taskService.getAllTaskCompleted();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(taskCompleted);
+    }
+
+    @GetMapping("/no-completed")
+    public ResponseEntity<List<TaskResponse>> getAllTaskNoCompleted() {
+        List<TaskResponse> taskNoCompleted = taskService.getAllTaskNoCompleted();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(taskNoCompleted);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateTask(@PathVariable Long id, @RequestBody @Valid TaskRequest task) {
+        taskService.updateTask(id, task);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<?> changeCompletedStatus(@PathVariable Long id, @RequestParam Boolean completed) {
+        taskService.changeCompletedStatus(id, completed);
+        return ResponseEntity.noContent().build();
+    }
 
 }
